adc6ea907102eb2a31246c471b36341b007ab225
parkjonghyeok2 <parkjonghyeok2000@gmail.com>
2022-05-24 13:17:51 +0900

    백준 문제

---
+++ JAVA/Backjoon/src/question/Bj3046.java
@@ -0,0 +1,16 @@
+package question;
+import java.util.Scanner;
+
+public class Bj3046 {
+	public static void main(String[] args) {
+		Scanner ex = new Scanner(System.in);
+		
+		int a = ex.nextInt();
+		int b = ex.nextInt();
+		
+		int result = b*2;
+		
+		System.out.println(result-a);
+	}
+
+}
--- JAVA/Backjoon/src/question/Bj5613.java
+++ JAVA/Backjoon/src/question/Bj5613.java
@@ -1,14 +1,34 @@
 package question;
 import java.util.Scanner;
-
+import java.util.StringTokenizer;
+import java.util.ArrayList;
 public class Bj5613 {
 	public static void main(String[] args) {
 		Scanner ex = new Scanner(System.in);
+		int result = ex.nextInt();
+		String a = "";
 		
-		int a  = ex.nextInt();
-		int b = ex.nextInt();
-		
-		int result;
-		
+		while(true) {
+			a= ex.next();
+			if(a.equals("=")) {
+				break;
+				}
+			
+			int num2 = ex.nextInt();			
+			if(a.equals("+")) {
+				result += num2; 
+			}
+			else if(a.equals("-")) {
+				result -=num2;
+			}
+			else if(a.equals("*")) {
+				result *=num2;
+			}
+			else if(a.equals("/")) {
+				result /= num2;
+			}
+		}
+		System.out.println(result);
 	}
 }
+
