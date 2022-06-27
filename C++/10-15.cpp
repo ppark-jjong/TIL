6e166f0eacc45e1f71818b760ad8ac9c207bd1e9
parkjonghyeok2 <parkjonghyeok2000@gmail.com>
2022-06-27 13:59:11 +0900

    파일정리

--- C++/10-15.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	[](int x, int y) {cout << "гую╨ " << x + y; }(2, 3);
-}
--- C++/9-2.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-class Base {
-public:
-	virtual void f() { cout << "Base::f() called" << endl; }
-};
