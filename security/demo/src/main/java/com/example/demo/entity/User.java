a44cb7a5046fd42f97b1771b8be37cdba5eeb76b
ppark__jjong <101304928+Parkjonghyeok2@users.noreply.github.com>
2023-12-29 03:46:23 +0900

    no message

---
+++ security/demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,25 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private String role;
+    @CreationTimestamp
+    private Timestamp createDate;
+
+}
